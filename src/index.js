f7605edb9de2d1c321603e99e74ec5b0811e0329
Shrihari <shrihari.uma.1999@gmail.com>
2021-05-08 19:24:48 +0530

    rendering from a list using map

--- src/index.js
+++ src/index.js
@@ -2,38 +2,29 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const firstBook = {
-  img:
-    "https://images-eu.ssl-images-amazon.com/images/I/91DtbV%2Bm2QS._AC_UL200_SR200,200_.jpg",
-  title: "Harry Potter and the Philosopher's Stone",
-  author: "J K Rowling",
-  rating: 4.5,
-};
-
-const secondBook = {
-  img:
-    "https://images-eu.ssl-images-amazon.com/images/I/61MrRA4qE0L._AC_UL200_SR200,200_.jpg",
-  title: "Indian Polity",
-  author: "M. Lakshminath",
-  rating: 4.0,
-};
+const books = [
+  {
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/91DtbV%2Bm2QS._AC_UL200_SR200,200_.jpg",
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J K Rowling",
+    rating: 4.5,
+  },
+  {
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/I/61MrRA4qE0L._AC_UL200_SR200,200_.jpg",
+    title: "Indian Polity",
+    author: "M. Lakshminath",
+    rating: 4.0,
+  },
+];
 
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-        rating={firstBook.rating}
-      />
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-        rating={secondBook.rating}
-      />
+      {books.map((book) => {
+        return <BookListComponent book={book} />;
+      })}
     </section>
   );
 };
@@ -45,6 +36,32 @@ const Book = (props) => {
       <h1 style={{ color: "red", fontSize: "large" }}>{props.title}</h1>
       <p> {props.author} </p>
       <p> rating : {props.rating} </p>
+    </div>
+  );
+};
+
+const BookAlternativeProp = ({ img, title, author, rating, children }) => {
+  //const { img, title, author } = props;
+  console.log(children);
+  return (
+    <div className="book">
+      <img className="bookImage" src={img}></img>
+      <h1 style={{ color: "red", fontSize: "large" }}>{title}</h1>
+      {children}
+      <p> {author} </p>
+      <p> rating : {rating} </p>
+    </div>
+  );
+};
+
+const BookListComponent = (props) => {
+  const { img, title, author, rating } = props.book;
+  return (
+    <div className="book">
+      <img className="bookImage" src={img}></img>
+      <h1 style={{ color: "red", fontSize: "large" }}>{title}</h1>
+      <p> {author} </p>
+      <p> rating : {rating} </p>
     </div>
   );
 };
