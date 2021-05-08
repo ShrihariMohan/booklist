4dee438b31756e896ea0ffe72e402626d1084f96
Shrihari <shrihari.uma.1999@gmail.com>
2021-05-08 17:56:00 +0530

    Introduction to props

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
 import React from "react";
 import ReactDom from "react-dom";
-
 import "./index.css";
+
+const firstBook = {
+  img:
+    "https://images-eu.ssl-images-amazon.com/images/I/91DtbV%2Bm2QS._AC_UL200_SR200,200_.jpg",
+  title: "Harry Potter and the Philosopher's Stone",
+  author: "J K Rowling",
+  rating: 4.5,
+};
+
+const secondBook = {
+  img:
+    "https://images-eu.ssl-images-amazon.com/images/I/61MrRA4qE0L._AC_UL200_SR200,200_.jpg",
+  title: "Indian Polity",
+  author: "M. Lakshminath",
+  rating: 4.0,
+};
+
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+        rating={firstBook.rating}
+      />
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        rating={secondBook.rating}
+      />
     </section>
   );
 };
 
-const Book = () => {
-  const h1text = "Harry Potter and the Philosopher's Stone on a variable";
+const Book = (props) => {
   return (
     <div className="book">
-      <img
-        className="bookImage"
-        src="https://images-eu.ssl-images-amazon.com/images/I/91DtbV%2Bm2QS._AC_UL200_SR200,200_.jpg"
-      ></img>
-      <h1 style={{ color: "red", fontSize: "large" }}>{h1text}</h1>
-      <p> J K Rowling</p>
+      <img className="bookImage" src={props.img}></img>
+      <h1 style={{ color: "red", fontSize: "large" }}>{props.title}</h1>
+      <p> {props.author} </p>
+      <p> rating : {props.rating} </p>
     </div>
   );
 };
